ab2f216a5cb9f1fe6590ef82239c1caf0e5a210a
Maksym Zhokha <maksym.zhokha@gmail.com>
2023-09-08 09:03:00 +0200

    Added requests.sh with useful commands.

---
+++ requests.sh
@@ -0,0 +1,5 @@
+curl http://localhost:8083/orderSearchService/order/phone?phoneNumber=123456789
+
+curl http://localhost:8082/productInfoService/product/names?productCode=5256
+
+curl -H "requestId:777" http://localhost:8080/userOrdersService/user/orders?userId=user1
